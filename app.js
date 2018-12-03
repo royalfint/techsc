41e8f40abfc1607c9bd303e0e95f63554cd8c178
Vlad Malikhtarovich <royalfint@hotmail.com>
2018-12-03 06:45:16 +0000

    contacts page

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-   res.render("home"); 
+   res.render("home", {light: false, page: "Главная"}); 
+});
+
+app.get("/contacts", function(req, res) {
+   res.render("contacts", {light: true, page: "Контакты"}); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
