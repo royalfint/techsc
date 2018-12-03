3c4f31e9041261e7d6483a2e155ec22e2bf227c6
Vlad Malikhtarovich <royalfint@hotmail.com>
2018-12-03 11:06:03 +0000

    all pages

--- app.js
+++ app.js
@@ -8,6 +8,22 @@ app.get("/", function(req, res) {
    res.render("home", {light: false, page: "Главная"}); 
 });
 
+app.get("/portfolio", function(req, res) {
+   res.render("portfolio", {light: true, page: "Портфолио"}); 
+});
+
+app.get("/blog", function(req, res) {
+   res.render("blog", {light: true, page: "Блог"}); 
+});
+
+app.get("/about", function(req, res) {
+   res.render("about", {light: true, page: "О Нас"}); 
+});
+
+app.get("/certificates", function(req, res) {
+   res.render("certificates", {light: true, page: "Аккредитация"}); 
+});
+
 app.get("/contacts", function(req, res) {
    res.render("contacts", {light: true, page: "Контакты"}); 
 });
