77184c9d270ffb966b7acf1768af92d6cd57dd61
Vlad Malikhtarovich <royalfint@hotmail.com>
2018-12-03 04:56:27 +0000

    heroku commit

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require("express");
 
 var app = express();
 app.set("view engine", "ejs");
+app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-   res.render("coming"); 
+   res.render("home"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
